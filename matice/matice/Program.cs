0b5189b11f83d1b2e463159182456d6b96924fc7
OndraBartos <ondra.bartos.g@gmail.com>
2023-11-17 19:02:36 +0100

    druhy ukol na prg

--- matice/matice/Program.cs
+++ matice/matice/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace matice
 {
@@ -34,21 +30,33 @@ namespace matice
             }
             Console.Write("\n");
 
+
             //PROHOZENÍ PRVKŮ 
-            Console.WriteLine("Prohození prvků v poli:");
+            Console.WriteLine("Zadej souřadnice čísla které budeš chtít měnít (nejdřív řádek, poté enter, pak sloupec)");
+            string u = Console.ReadLine();
+            int xfirst = int.Parse(u) - 1;  //musím dát u každého - 1, protože index je vždy o jedno menší než velikost řádku/sloupce
+            string v = Console.ReadLine();  //tzn. že kdybych tam nedal - 1, tak mi to spadne pokaždý co budu chtít měnit nějakou souřadnici na horním kraji pole
+            int yfirst = int.Parse(v) - 1;  //to samé u prohazování řádků a sloupců viz. dole
+            Console.WriteLine("Nyní zadej souřadnice čísla se kterým ho budeš chtít měnít (nejdřív řádek, poté enter, pak sloupec)"); //to samé u sloupců
+            string s = Console.ReadLine();
+            int xsecond = int.Parse(s) - 1;
+            string t = Console.ReadLine();
+            int ysecond = int.Parse(t) - 1;
+            Console.Write("\n");
+
+            Console.WriteLine("Prohození zvolených prvků v poli:");
             //int[,] tempArray1 = new int[Row1, Column1];   překrývali se mi operace a snažil jsem se nějak vymyslet jak to opravit
             //napadlo mě že by to šlo udělat pomocí zkopírování původního pole do dočasného pole pro každý úkol zvlášť (podobně jako s jednotlivými oprvky)
             int[,] tempArray1 = (int[,])firstArray.Clone(); //musel jsem si najít na chat gtp jak si to meziuložit do tempArray
-            int xfirst = 0;
-            int yfirst = 0;                            //nejdřív si meziuložím prvnmí číslo na temporary/dočasnou proměnou, potom to první číslo přepíšu druhým číslem
-            int xsecond = 1;                           //potom přepíšu druhé číslo dočasným číslem, které má hodnotu prvního čísla
-            int ysecond = 1;
+            //int xfirst = 0;
+            //int yfirst = 0;                       //nejdřív si meziuložím prvnmí číslo na temporary/dočasnou proměnou, potom to první číslo přepíšu druhým číslem
+            //int xsecond = 1;                      //potom přepíšu druhé číslo dočasným číslem, které má hodnotu prvního čísla
+            //int ysecond = 1;
             int first = tempArray1[xfirst, yfirst];
             int second = tempArray1[xsecond, ysecond];
             int temp = first;
             tempArray1[xfirst, yfirst] = second;
             tempArray1[xsecond, ysecond] = temp;
-
             for (int i = 0; i < tempArray1.GetLength(0); i++) //vypsání do konzole
             {
                 for (int j = 0; j < tempArray1.GetLength(1); j++)
@@ -59,28 +67,33 @@ namespace matice
             }
             Console.Write("\n");
 
+
             //PROHOZENÍ ŘÁDKŮ
+            Console.WriteLine("Zadej řádek, který chceš prohodit");
+            string k = Console.ReadLine();
+            int nRowSwap = int.Parse(k) - 1;
+            Console.WriteLine("Zadej řádek, se kterým ho chceš prohodit");
+            string l = Console.ReadLine();
+            int mRowSwap = int.Parse(l) - 1;
+            Console.Write("\n");
+
             Console.WriteLine("Prohození řádků v poli:");
             int[,] tempArray2 = (int[,])firstArray.Clone(); //musím si vytvořit novou pro každou operaci
-            int nRowSwap = 0; //který řádek prohazujeme
-            int mRowSwap = 1; //za který ho chceme prohodit
-            int[] tempArrayRow = new int[5]; //podobně jako u císla chci přesunout celý řádek, proto si ho musím meziuložit v dočasném poli
-
+            //int nRowSwap = 0; //který řádek prohazujeme
+            //int mRowSwap = 1; //za který ho chceme prohodit
+            int[] tempArrayRow = new int[Row1]; //podobně jako u čísla chci přesunout celý řádek, proto si ho musím meziuložit v dočasném poli
             for (int j = 0; j < tempArray2.GetLength(0); j++) //zkopíruju nRowSwap do pomocného pole tempArray
             {
                 tempArrayRow[j] = tempArray2[nRowSwap, j];
             }
-
             for (int j = 0; j < tempArray2.GetLength(0); j++) //přepíšu nRowSwap na mRowSwap
             {
                 tempArray2[nRowSwap, j] = tempArray2[mRowSwap, j];
             }
-
             for (int j = 0; j < tempArray2.GetLength(0); j++) //přepíšu mRowSwap pomocným polem tempArray
             {
                 tempArray2[mRowSwap, j] = tempArrayRow[j];
             }
-
             for (int i = 0; i < tempArray2.GetLength(0); i++) //vypsání do konzole
             {
                 for (int j = 0; j < tempArray2.GetLength(1); j++)
@@ -91,28 +104,33 @@ namespace matice
             }
             Console.Write("\n");
 
+
             //PROHAZOVÁNÍ SLOUPCŮ
+            Console.WriteLine("Zadej řádek, který chceš prohodit)");            //to samé co u řádků udělám u sloupců
+            string m = Console.ReadLine();
+            int nColSwap = int.Parse(m) - 1;
+            Console.WriteLine("Zadej řádek, se kterým ho chceš prohodit)");
+            string n = Console.ReadLine();
+            int mColSwap = int.Parse(n) - 1;
+            Console.Write("\n");
+
             Console.WriteLine("Prohození sloupců v poli:");
             int[,] tempArray3 = (int[,])firstArray.Clone();
-            int nColSwap = 0;
-            int mColSwap = 1;
-            int[] tempArrayColumn = new int[5];
-
+            //int nColSwap = 0;                                   
+            //int mColSwap = 1;
+            int[] tempArrayColumn = new int[Column1];
             for (int i = 0; i < tempArray3.GetLength(0); i++) //ulozím první sloupec do dočasného pole
             {
                 tempArrayColumn[i] = tempArray3[i, nColSwap];
             }
-
             for (int i = 0; i < tempArray3.GetLength(0); i++) //přepíšu první sloupec druhým sloupcem
             {
                 tempArray3[i, nColSwap] = tempArray3[i, mColSwap];
             }
-
             for (int i = 0; i < tempArray3.GetLength(0); i++) //přepíšu druhý sloupec dočasným polem
             {
                 tempArray3[i, mColSwap] = tempArrayColumn[i];
             }
-
             for (int i = 0; i < tempArray3.GetLength(0); i++) //vypsání do konzole
             {
                 for (int j = 0; j < tempArray3.GetLength(1); j++)
@@ -122,6 +140,7 @@ namespace matice
                 Console.Write("\n");
             }
             Console.Write("\n");
+
 
             //PROHOZENÍ POŘADÍ HLAVNÍ DIAGONÁLY
             Console.WriteLine("Prohození pořadí prvků na hlavní diagonále v poli:");
@@ -134,7 +153,6 @@ namespace matice
                 tempArray4[i, i] = tempArray4[reversindex, reversindex];
                 tempArray4[reversindex, reversindex] = temporary;
             }
-
             for (int i = 0; i < tempArray4.GetLength(0); i++) //vypsání do konzole
             {
                 for (int j = 0; j < tempArray4.GetLength(1); j++)
@@ -144,6 +162,7 @@ namespace matice
                 Console.Write("\n");
             }
             Console.Write("\n");
+
 
             //PROHOZENÍ POŘADÍ VEDLEJŠÍ DIAGONÁLY
             Console.WriteLine("Prohození pořadí prvků na vedlejší diagonále v poli:");
@@ -155,7 +174,6 @@ namespace matice
                 tempArray5[i, j] = tempArray5[j, i]; //zrcadlově prohodím souřadnice
                 tempArray5[j, i] = temporary2;
             }
-
             for (int i = 0; i < tempArray5.GetLength(0); i++) //vypsání do konzole
             {
                 for (int j = 0; j < tempArray5.GetLength(1); j++)
@@ -165,6 +183,7 @@ namespace matice
                 Console.Write("\n");
             }
             Console.Write("\n");
+
 
             //NÁSOBENÍ MATICE ČÍSLEM
             Console.WriteLine("Zadejte číslo, kterým chcete matici vynásobit");
@@ -178,6 +197,7 @@ namespace matice
                     tempArray6[i, j] *= multiplier; // *= zkratka pro násobení a přiřazení, nyvásobí hodnotu i a j tím co je za rovná se
                 }
             }
+            Console.WriteLine("Zde je původní matice vynásobená vámi zvoleným číslem:");
             for (int i = 0; i < tempArray6.GetLength(0); i++) //vypsání do konzole
             {
                 for (int j = 0; j < tempArray6.GetLength(1); j++)
@@ -189,24 +209,27 @@ namespace matice
             Console.Write("\n");
 
 
-
-
-            //druhá matice na další operace
-            Console.WriteLine("Zadej počet řádků druhé matice");
-            string c = Console.ReadLine();
-            int Row2 = int.Parse(c);
-            Console.WriteLine("Zadej počet sloupců druhé matice");
-            string d = Console.ReadLine();
-            int Column2 = int.Parse(d);
-
-            int[,] secondArray = new int[Row2, Column2];
-            int numberToAdd2 = 1;
+            //DRUHÁ MATICE NA DALŠÍ OPERACE
+            //Console.WriteLine("Zadej počet řádků druhé matice");
+            //string c = Console.ReadLine();                               nejdřív jsem chtěl ať si druhou matici opět zadá uživatel
+            //int Row2 = int.Parse(c);                                     pak mi ale došlo že musí mít stejnou velikost
+            //Console.WriteLine("Zadej počet sloupců druhé matice");
+            //string d = Console.ReadLine();
+            //int Column2 = int.Parse(d);
+            int[,] secondArray = new int[Row1, Column1]; //druhá matice, která bude mít stejně řádků a sloupců jako první matice
+            Random rng = new Random();
             for (int i = 0; i < secondArray.GetLength(0); i++)
             {
                 for (int j = 0; j < secondArray.GetLength(1); j++)
                 {
-                    secondArray[i, j] = numberToAdd2;
-                    numberToAdd2++;
+                    secondArray[i, j] = rng.Next(1, Row1 * Column1);
+                }   //naplním ji náhodnými čísly v rozmezí od 1 do maximální velikosti matice(Row*Column)
+            }
+            Console.WriteLine("Zde je druhá matice naplněná náhodnými čísly:");
+            for (int i = 0; i < secondArray.GetLength(0); i++) //vypsání druhé matice
+            {
+                for (int j = 0; j < secondArray.GetLength(1); j++)
+                {
                     Console.Write(secondArray[i, j] + " ");
                 }
                 Console.Write("\n");
@@ -214,6 +237,105 @@ namespace matice
             Console.Write("\n");
 
 
+            //SČÍTÁNÍ/ODČÍTÁNÍ DVOU MATIC
+            Console.WriteLine("Chcete od sebe matice sčítat, nebo odčítat?");
+            Console.WriteLine("(pro sčítaní zadejte +)");
+            Console.WriteLine("(pro odčítání zadejte -)");
+            int[,] tempArray7 = (int[,])firstArray.Clone();
+            int[,] tempArray8 = (int[,])firstArray.Clone();
+            string choice = Console.ReadLine();
+            //choice = Console.ReadLine();
+            if (choice == "+")
+            {
+                for (int i = 0; i < tempArray7.GetLength(0); i++)
+                {
+                    for (int j = 0; j < tempArray7.GetLength(1); j++)
+                    {
+                        tempArray7[i, j] = firstArray[i, j] + secondArray[i, j]; //výsledná matice se rovná první matice + druhá matice
+                    }
+                }
+                Console.WriteLine("Zde je výsledná matice po sčítání:");
+                for (int i = 0; i < tempArray7.GetLength(0); i++) //vypsání do konzole
+                {
+                    for (int j = 0; j < tempArray7.GetLength(1); j++)
+                    {
+                        Console.Write(tempArray7[i, j] + " ");
+                    }
+                    Console.Write("\n");
+                }
+                Console.Write("\n");
+            }
+            else if (choice == "-")
+            {
+                for (int i = 0; i < tempArray8.GetLength(0); i++)
+                {
+                    for (int j = 0; j < tempArray8.GetLength(1); j++)
+                    {
+                        tempArray8[i, j] = firstArray[i, j] - secondArray[i, j]; //výsledná matice se rovná první matice - druhá matice
+                    }
+                }
+                Console.WriteLine("Zde je výsledná matice po odčítání:");
+                for (int i = 0; i < tempArray8.GetLength(0); i++) //vypsání do konzole
+                {
+                    for (int j = 0; j < tempArray8.GetLength(1); j++)
+                    {
+                        Console.Write(tempArray8[i, j] + " ");
+                    }
+                    Console.Write("\n");
+                }
+                Console.Write("\n");
+            }
+            else
+            {
+                Console.WriteLine("ŠPATNĚ ZADANÁ MOŽNOST OPERACE");
+            }
+
+
+            //TRANSPOZICE MATICE
+            int[,] tempArray9 = (int[,])firstArray.Clone();
+            for (int i = 0; i < tempArray9.GetLength(0); i++)
+            {
+                for (int j = 0; j < tempArray9.GetLength(1); j++)
+                {
+                    tempArray9[i, j] = firstArray[j, i]; //v nové matici se prvek na pozici [1, 2] rovná prvku v originální matici na pozici [2, 1]
+                }
+            }
+            Console.WriteLine("Zde je výsledná matice po transpozici = převrácení kolem hlavní diagonály:");
+            for (int i = 0; i < tempArray9.GetLength(0); i++) //vypsání do konzole
+            {
+                for (int j = 0; j < tempArray9.GetLength(1); j++)
+                {
+                    Console.Write(tempArray9[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+
+
+            //NÁSOBENÍ DVOU MATIC
+            //nevím jestli jsem to pochopil špatně, ale je to úplně stejný jako to sčítání a odčítání
+            int[,] tempArray10 = (int[,])firstArray.Clone();
+            for (int i = 0; i < tempArray10.GetLength(0); i++)
+            {
+                for (int j = 0; j < tempArray10.GetLength(1); j++)
+                {
+                    tempArray10[i, j] = firstArray[i, j] * secondArray[i, j]; //výsledná matice se rovná první matice * druhá matice
+                }
+            }
+            Console.WriteLine("Zde je výsledná matice po násobení:");
+            for (int i = 0; i < tempArray10.GetLength(0); i++) //vypsání do konzole
+            {
+                for (int j = 0; j < tempArray10.GetLength(1); j++)
+                {
+                    Console.Write(tempArray10[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+
+
+            Console.WriteLine("KONEC, stiskněte jakoukoli klávesu pro ukončení");
+            //program čěká na ukončení
             Console.ReadKey();
         }
     }
--- matice/matice/Properties/AssemblyInfo.cs
+++ matice/matice/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("74796f16-ec82-4721-b1dc-318f000dd89a")]
+[assembly: Guid("6750fbd8-6c20-4860-9332-c0144ea52b14")]
 
 // Version information for an assembly consists of the following four values:
 //
