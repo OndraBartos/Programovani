92914dddbed5ebee8925ac02fd9a51ebc2af3777
Ondra Bartos <ondra.bartos.g@gmail.com>
2024-02-06 11:24:12 +0100

    update z hodiny

---
+++ GraphPlayground/GraphPlayground/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace GraphPlayground
+{
+    internal class Program
+    {
+        public static void DFS(Graph graph, Node startNode, Node targetNode = null)
+        {
+            Node currentNode = startNode;
+            while(true)
+            {
+                Node neighbourToVisit = null;
+                foreach(Node neighbour in currentNode.neighbors)
+                {
+                    if (!neighbour.visited)
+                    {
+                        neighbourToVisit = neighbour;
+                        break;
+                    }
+                }
+                if (neighbourToVisit == null) //všichni sousedi jsou už navštíveni
+                {
+                    if (currentNode == startNode)
+                    {
+                        Console.WriteLine("jsem ve startovacím uzlu a nemám kam jít");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"jsem ve slepé uličce, vracím se z uzlu {currentNode.index} do uzlu {currentNode.cameFrom.index}");
+                        currentNode = currentNode.cameFrom;
+                    }
+                }
+                else //mám nenavštíveného souseda kterého jdu navštívit
+                {
+                    Console.WriteLine($"jduz uzlu {currentNode.index} do uzlu {neighbourToVisit.index}");
+                    neighbourToVisit.visited = true;
+                    neighbourToVisit.cameFrom = currentNode;
+                    currentNode = neighbourToVisit;
+                }
+            }
+        }
+
+        public static void BFS(Graph graph, Node startNode, Node targetNode = null) 
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            //Create and print the graph
+            Graph graph = new Graph();
+            graph.PrintGraph();
+            graph.PrintGraphForVisualization();
+
+            //Call both algorithms with a random starting node
+            Random rng = new Random();
+            DFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+            BFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Procvicovani/Procvicovani/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procvicovani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine(); //načtu první číslo od uživatele
+            int num1 = int.Parse(a);
+
+            string b = Console.ReadLine(); //načtu druhé číslo od uživatele
+            int num2 = int.Parse(b);
+
+            Console.WriteLine($"tvoje čísla: {num1}, {num2}"); //vypsání do konzole
+
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+            int sub = num1 - num2;
+            Console.WriteLine(sub);
+            int multiply = num2 * num1;
+            Console.WriteLine(multiply);
+            float divide = (float)num1 / (float)num2;
+            Console.WriteLine(divide);
+
+            //Console.WriteLine($"{sum}, {sub}, {multiply}, {divide}");
+
+            float exp = (float)Math.Exp(divide);
+            float sqrt = (float)Math.Sqrt(divide);
+
+            Console.WriteLine($"{exp}, {sqrt}");
+
+            if (sum >= 0)
+            {
+                Console.WriteLine("suma je kladná");
+            }
+            else
+            {
+                Console.WriteLine("suma je záporná");
+            }
+
+            if (sub > multiply)
+            {
+                Console.WriteLine("rozdílje větší než násobek");
+            }
+            else
+            {
+                Console.WriteLine("rozdílje menší než násobek");
+            }
+
+            if (sum > sub && multiply > divide)
+            {
+                Console.WriteLine("sum je větší než sub + multiply je větší než divide");
+            }
+            else
+            {
+                Console.WriteLine("třetí if neplatí");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
