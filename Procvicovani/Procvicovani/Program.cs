5736dba7f324e5b6712d8ed5262d8f98271eb65c
OndraBartos <ondra.bartos.g@gmail.com>
2024-02-19 21:29:09 +0100

    práce z domova

--- Procvicovani/Procvicovani/Program.cs
+++ Procvicovani/Procvicovani/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -45,11 +46,11 @@ namespace Procvicovani
 
             if (sub > multiply)
             {
-                Console.WriteLine("rozdílje větší než násobek");
+                Console.WriteLine("rozdíl je větší než násobek");
             }
             else
             {
-                Console.WriteLine("rozdílje menší než násobek");
+                Console.WriteLine("rozdíl je menší než násobek");
             }
 
             if (sum > sub && multiply > divide)
@@ -60,6 +61,87 @@ namespace Procvicovani
             {
                 Console.WriteLine("třetí if neplatí");
             }
+
+            Console.WriteLine("zadej velikost pole");
+            string width = Console.ReadLine();
+            int x = int.Parse(width);
+            int[] numArray = new int[x]; //vytvořím nové pole o velikosti 100 
+            int indexOfArray = 0; //nastavím počáteční index pole na nulu
+            char[] charArray = new char[x - 1]; //velikost char array je o jedno menší než velikost pole čísel
+            int charIndex = 0;
+
+            Console.WriteLine("nyní můžete psát čísla, pro ukončení napište stop");
+
+            while (true)
+            {               
+                string command = Console.ReadLine(); //máme jak pro čísla tak pro stop
+
+                if (command == "stop") //ukončí cyklus pokud napíše stop
+                { 
+                    break;
+                }
+                if (indexOfArray >= numArray.Length) //ukončí cyklus pokud už máme hodně čísel a nestačí nám pole
+                {
+                    Console.WriteLine("Pole je plné, nelze zadat více čísel.");
+                    break;
+                }
+                
+                if (int.TryParse(command, out int cislo))//pokud uživatel zadal číslo, převedeme ho na integer a uložíme do pole
+                {
+                    numArray[indexOfArray] = cislo;
+                    indexOfArray++;
+                }
+                else //pokud nezadá číslo nebo stop
+                {
+                    Console.WriteLine("Neplatný vstup, zadej platné číslo nebo 'stop'.");
+                }
+            }
+            Console.WriteLine("Zadána čísla:");
+            for (int i = 0; i < indexOfArray; i++)
+            {
+                Console.Write(numArray[i] + " ");
+            }
+
+            while (true)
+            {
+                string vstup = Console.ReadLine();
+
+                // Pokud je pole operací plné, informujeme uživatele a ukončíme cyklus
+                if (charIndex >= charArray.Length)
+                {
+                    Console.WriteLine("Pole operací je plné, nelze zadat více operací.");
+                    break;
+                }
+
+                // Pokud uživatel zadal platnou operaci (+ nebo -), uložíme ji do pole operací
+                if (vstup == "+" || vstup == "-")
+                {
+                    charArray[charIndex] = vstup[0]; // Ukládáme jen první znak
+                    charIndex++;
+                }
+            }
+
+            // Pokud byla zadána chybná kombinace čísel a operací, ukončíme program
+            if (indexOfArray != charIndex + 1)
+            {
+                Console.WriteLine("Chybná kombinace čísel a operací.");
+                return;
+            }
+
+            int vysledek = numArray[0];
+            for (int i = 0; i < charIndex; i++)
+            {
+                if (charArray[i] == '+')
+                {
+                    vysledek += numArray[i + 1];
+                }
+                else if (charArray[i] == '-')
+                {
+                    vysledek -= numArray[i + 1];
+                }
+            }
+
+            Console.WriteLine("Výsledek: " + vysledek);
 
 
 
