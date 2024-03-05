3a970942aed607be83a20ad95c377bf77aff65bb
Ondra Bartos <ondra.bartos.g@gmail.com>
2024-03-05 11:47:04 +0100

    update z hodiny

---
+++ MalovaniUkol/MalovaniUkol/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace MalovaniUkol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawingpanel = new System.Windows.Forms.Panel();
+            this.colorBlack = new System.Windows.Forms.Button();
+            this.colorCyan = new System.Windows.Forms.Button();
+            this.colorPurple = new System.Windows.Forms.Button();
+            this.colorBlue = new System.Windows.Forms.Button();
+            this.colorPink = new System.Windows.Forms.Button();
+            this.colorWhite = new System.Windows.Forms.Button();
+            this.colorGreen = new System.Windows.Forms.Button();
+            this.colorRed = new System.Windows.Forms.Button();
+            this.colorOrange = new System.Windows.Forms.Button();
+            this.colorYellow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.newPage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // drawingpanel
+            // 
+            this.drawingpanel.BackColor = System.Drawing.Color.White;
+            this.drawingpanel.Location = new System.Drawing.Point(194, 22);
+            this.drawingpanel.Name = "drawingpanel";
+            this.drawingpanel.Size = new System.Drawing.Size(763, 608);
+            this.drawingpanel.TabIndex = 0;
+            // 
+            // colorBlack
+            // 
+            this.colorBlack.BackColor = System.Drawing.Color.Black;
+            this.colorBlack.Location = new System.Drawing.Point(25, 40);
+            this.colorBlack.Name = "colorBlack";
+            this.colorBlack.Size = new System.Drawing.Size(20, 18);
+            this.colorBlack.TabIndex = 2;
+            this.colorBlack.UseVisualStyleBackColor = false;
+            // 
+            // colorCyan
+            // 
+            this.colorCyan.BackColor = System.Drawing.Color.Cyan;
+            this.colorCyan.Location = new System.Drawing.Point(25, 64);
+            this.colorCyan.Name = "colorCyan";
+            this.colorCyan.Size = new System.Drawing.Size(20, 18);
+            this.colorCyan.TabIndex = 3;
+            this.colorCyan.UseVisualStyleBackColor = false;
+            // 
+            // colorPurple
+            // 
+            this.colorPurple.BackColor = System.Drawing.Color.Purple;
+            this.colorPurple.Location = new System.Drawing.Point(25, 112);
+            this.colorPurple.Name = "colorPurple";
+            this.colorPurple.Size = new System.Drawing.Size(20, 18);
+            this.colorPurple.TabIndex = 4;
+            this.colorPurple.UseVisualStyleBackColor = false;
+            // 
+            // colorBlue
+            // 
+            this.colorBlue.BackColor = System.Drawing.Color.Blue;
+            this.colorBlue.Location = new System.Drawing.Point(25, 88);
+            this.colorBlue.Name = "colorBlue";
+            this.colorBlue.Size = new System.Drawing.Size(20, 18);
+            this.colorBlue.TabIndex = 5;
+            this.colorBlue.UseVisualStyleBackColor = false;
+            // 
+            // colorPink
+            // 
+            this.colorPink.BackColor = System.Drawing.Color.Fuchsia;
+            this.colorPink.Location = new System.Drawing.Point(25, 136);
+            this.colorPink.Name = "colorPink";
+            this.colorPink.Size = new System.Drawing.Size(20, 18);
+            this.colorPink.TabIndex = 6;
+            this.colorPink.UseVisualStyleBackColor = false;
+            // 
+            // colorWhite
+            // 
+            this.colorWhite.BackColor = System.Drawing.Color.White;
+            this.colorWhite.Location = new System.Drawing.Point(51, 40);
+            this.colorWhite.Name = "colorWhite";
+            this.colorWhite.Size = new System.Drawing.Size(20, 18);
+            this.colorWhite.TabIndex = 7;
+            this.colorWhite.UseVisualStyleBackColor = false;
+            // 
+            // colorGreen
+            // 
+            this.colorGreen.BackColor = System.Drawing.Color.Lime;
+            this.colorGreen.Location = new System.Drawing.Point(51, 64);
+            this.colorGreen.Name = "colorGreen";
+            this.colorGreen.Size = new System.Drawing.Size(20, 18);
+            this.colorGreen.TabIndex = 8;
+            this.colorGreen.UseVisualStyleBackColor = false;
+            // 
+            // colorRed
+            // 
+            this.colorRed.BackColor = System.Drawing.Color.Red;
+            this.colorRed.Location = new System.Drawing.Point(50, 136);
+            this.colorRed.Name = "colorRed";
+            this.colorRed.Size = new System.Drawing.Size(20, 18);
+            this.colorRed.TabIndex = 9;
+            this.colorRed.UseVisualStyleBackColor = false;
+            // 
+            // colorOrange
+            // 
+            this.colorOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.colorOrange.Location = new System.Drawing.Point(50, 112);
+            this.colorOrange.Name = "colorOrange";
+            this.colorOrange.Size = new System.Drawing.Size(20, 18);
+            this.colorOrange.TabIndex = 10;
+            this.colorOrange.UseVisualStyleBackColor = false;
+            // 
+            // colorYellow
+            // 
+            this.colorYellow.BackColor = System.Drawing.Color.Yellow;
+            this.colorYellow.Location = new System.Drawing.Point(50, 88);
+            this.colorYellow.Name = "colorYellow";
+            this.colorYellow.Size = new System.Drawing.Size(20, 18);
+            this.colorYellow.TabIndex = 11;
+            this.colorYellow.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "vyber si barvu";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(15, 202);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(143, 25);
+            this.hScrollBar1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "tloušťka čáry";
+            // 
+            // newPage
+            // 
+            this.newPage.Location = new System.Drawing.Point(104, 88);
+            this.newPage.Name = "newPage";
+            this.newPage.Size = new System.Drawing.Size(53, 41);
+            this.newPage.TabIndex = 15;
+            this.newPage.Text = "nový papír";
+            this.newPage.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 659);
+            this.Controls.Add(this.newPage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.colorYellow);
+            this.Controls.Add(this.colorOrange);
+            this.Controls.Add(this.colorRed);
+            this.Controls.Add(this.colorGreen);
+            this.Controls.Add(this.colorWhite);
+            this.Controls.Add(this.colorPink);
+            this.Controls.Add(this.colorBlue);
+            this.Controls.Add(this.colorPurple);
+            this.Controls.Add(this.colorCyan);
+            this.Controls.Add(this.colorBlack);
+            this.Controls.Add(this.drawingpanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel drawingpanel;
+        private System.Windows.Forms.Button colorBlack;
+        private System.Windows.Forms.Button colorCyan;
+        private System.Windows.Forms.Button colorPurple;
+        private System.Windows.Forms.Button colorBlue;
+        private System.Windows.Forms.Button colorPink;
+        private System.Windows.Forms.Button colorWhite;
+        private System.Windows.Forms.Button colorGreen;
+        private System.Windows.Forms.Button colorRed;
+        private System.Windows.Forms.Button colorOrange;
+        private System.Windows.Forms.Button colorYellow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button newPage;
+    }
+}
+
