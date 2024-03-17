7ca0f80a4cb645092f270f2a6b5b465f1212d80d
OndraBartos <ondra.bartos.g@gmail.com>
2024-03-17 21:51:34 +0100

    práce z domova

--- MalovaniUkol/MalovaniUkol/Form1.Designer.cs
+++ MalovaniUkol/MalovaniUkol/Form1.Designer.cs
@@ -28,71 +28,229 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.drawingpanel = new System.Windows.Forms.Panel();
+            this.optionsPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.buttonCircle = new System.Windows.Forms.Button();
+            this.buttonRectangle = new System.Windows.Forms.Button();
+            this.buttonEraser = new System.Windows.Forms.Button();
+            this.buttonPen = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.newPageButton = new System.Windows.Forms.Button();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
             this.colorBlack = new System.Windows.Forms.Button();
+            this.colorYellow = new System.Windows.Forms.Button();
             this.colorCyan = new System.Windows.Forms.Button();
+            this.colorOrange = new System.Windows.Forms.Button();
             this.colorPurple = new System.Windows.Forms.Button();
+            this.colorRed = new System.Windows.Forms.Button();
             this.colorBlue = new System.Windows.Forms.Button();
+            this.colorGreen = new System.Windows.Forms.Button();
             this.colorPink = new System.Windows.Forms.Button();
             this.colorWhite = new System.Windows.Forms.Button();
-            this.colorGreen = new System.Windows.Forms.Button();
-            this.colorRed = new System.Windows.Forms.Button();
-            this.colorOrange = new System.Windows.Forms.Button();
-            this.colorYellow = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
-            this.label2 = new System.Windows.Forms.Label();
-            this.newPage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.optionsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // drawingpanel
+            // optionsPanel
             // 
-            this.drawingpanel.BackColor = System.Drawing.Color.White;
-            this.drawingpanel.Location = new System.Drawing.Point(194, 22);
-            this.drawingpanel.Name = "drawingpanel";
-            this.drawingpanel.Size = new System.Drawing.Size(763, 608);
-            this.drawingpanel.TabIndex = 0;
+            this.optionsPanel.BackColor = System.Drawing.Color.Silver;
+            this.optionsPanel.Controls.Add(this.button1);
+            this.optionsPanel.Controls.Add(this.button3);
+            this.optionsPanel.Controls.Add(this.buttonCircle);
+            this.optionsPanel.Controls.Add(this.buttonRectangle);
+            this.optionsPanel.Controls.Add(this.buttonEraser);
+            this.optionsPanel.Controls.Add(this.buttonPen);
+            this.optionsPanel.Controls.Add(this.label2);
+            this.optionsPanel.Controls.Add(this.newPageButton);
+            this.optionsPanel.Controls.Add(this.hScrollBar1);
+            this.optionsPanel.Controls.Add(this.label1);
+            this.optionsPanel.Controls.Add(this.colorBlack);
+            this.optionsPanel.Controls.Add(this.colorYellow);
+            this.optionsPanel.Controls.Add(this.colorCyan);
+            this.optionsPanel.Controls.Add(this.colorOrange);
+            this.optionsPanel.Controls.Add(this.colorPurple);
+            this.optionsPanel.Controls.Add(this.colorRed);
+            this.optionsPanel.Controls.Add(this.colorBlue);
+            this.optionsPanel.Controls.Add(this.colorGreen);
+            this.optionsPanel.Controls.Add(this.colorPink);
+            this.optionsPanel.Controls.Add(this.colorWhite);
+            this.optionsPanel.Location = new System.Drawing.Point(12, 12);
+            this.optionsPanel.Name = "optionsPanel";
+            this.optionsPanel.Size = new System.Drawing.Size(956, 84);
+            this.optionsPanel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(612, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 41);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(671, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(53, 41);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // buttonCircle
+            // 
+            this.buttonCircle.Location = new System.Drawing.Point(494, 19);
+            this.buttonCircle.Name = "buttonCircle";
+            this.buttonCircle.Size = new System.Drawing.Size(53, 41);
+            this.buttonCircle.TabIndex = 18;
+            this.buttonCircle.Text = "Kruh";
+            this.buttonCircle.UseVisualStyleBackColor = true;
+            this.buttonCircle.Click += new System.EventHandler(this.buttonCircle_Click);
+            // 
+            // buttonRectangle
+            // 
+            this.buttonRectangle.Location = new System.Drawing.Point(553, 19);
+            this.buttonRectangle.Name = "buttonRectangle";
+            this.buttonRectangle.Size = new System.Drawing.Size(53, 41);
+            this.buttonRectangle.TabIndex = 19;
+            this.buttonRectangle.Text = "Čtverec";
+            this.buttonRectangle.UseVisualStyleBackColor = true;
+            this.buttonRectangle.Click += new System.EventHandler(this.buttonRectangle_Click);
+            // 
+            // buttonEraser
+            // 
+            this.buttonEraser.Location = new System.Drawing.Point(435, 19);
+            this.buttonEraser.Name = "buttonEraser";
+            this.buttonEraser.Size = new System.Drawing.Size(53, 41);
+            this.buttonEraser.TabIndex = 17;
+            this.buttonEraser.Text = "Guma";
+            this.buttonEraser.UseVisualStyleBackColor = true;
+            this.buttonEraser.Click += new System.EventHandler(this.buttonEraser_Click);
+            // 
+            // buttonPen
+            // 
+            this.buttonPen.Location = new System.Drawing.Point(376, 19);
+            this.buttonPen.Name = "buttonPen";
+            this.buttonPen.Size = new System.Drawing.Size(53, 41);
+            this.buttonPen.TabIndex = 16;
+            this.buttonPen.Text = "Pero";
+            this.buttonPen.UseVisualStyleBackColor = true;
+            this.buttonPen.Click += new System.EventHandler(this.buttonPen_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "tloušťka čáry";
+            // 
+            // newPageButton
+            // 
+            this.newPageButton.Location = new System.Drawing.Point(317, 19);
+            this.newPageButton.Name = "newPageButton";
+            this.newPageButton.Size = new System.Drawing.Size(53, 41);
+            this.newPageButton.TabIndex = 15;
+            this.newPageButton.Text = "nový papír";
+            this.newPageButton.UseVisualStyleBackColor = true;
+            this.newPageButton.Click += new System.EventHandler(this.newPageButton_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(159, 35);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(143, 25);
+            this.hScrollBar1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "vyber si barvu";
             // 
             // colorBlack
             // 
             this.colorBlack.BackColor = System.Drawing.Color.Black;
-            this.colorBlack.Location = new System.Drawing.Point(25, 40);
+            this.colorBlack.Location = new System.Drawing.Point(18, 30);
             this.colorBlack.Name = "colorBlack";
             this.colorBlack.Size = new System.Drawing.Size(20, 18);
             this.colorBlack.TabIndex = 2;
             this.colorBlack.UseVisualStyleBackColor = false;
             // 
+            // colorYellow
+            // 
+            this.colorYellow.BackColor = System.Drawing.Color.Yellow;
+            this.colorYellow.Location = new System.Drawing.Point(70, 54);
+            this.colorYellow.Name = "colorYellow";
+            this.colorYellow.Size = new System.Drawing.Size(20, 18);
+            this.colorYellow.TabIndex = 11;
+            this.colorYellow.UseVisualStyleBackColor = false;
+            // 
             // colorCyan
             // 
             this.colorCyan.BackColor = System.Drawing.Color.Cyan;
-            this.colorCyan.Location = new System.Drawing.Point(25, 64);
+            this.colorCyan.Location = new System.Drawing.Point(44, 30);
             this.colorCyan.Name = "colorCyan";
             this.colorCyan.Size = new System.Drawing.Size(20, 18);
             this.colorCyan.TabIndex = 3;
             this.colorCyan.UseVisualStyleBackColor = false;
             // 
+            // colorOrange
+            // 
+            this.colorOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.colorOrange.Location = new System.Drawing.Point(96, 54);
+            this.colorOrange.Name = "colorOrange";
+            this.colorOrange.Size = new System.Drawing.Size(20, 18);
+            this.colorOrange.TabIndex = 10;
+            this.colorOrange.UseVisualStyleBackColor = false;
+            // 
             // colorPurple
             // 
             this.colorPurple.BackColor = System.Drawing.Color.Purple;
-            this.colorPurple.Location = new System.Drawing.Point(25, 112);
+            this.colorPurple.Location = new System.Drawing.Point(96, 30);
             this.colorPurple.Name = "colorPurple";
             this.colorPurple.Size = new System.Drawing.Size(20, 18);
             this.colorPurple.TabIndex = 4;
             this.colorPurple.UseVisualStyleBackColor = false;
             // 
+            // colorRed
+            // 
+            this.colorRed.BackColor = System.Drawing.Color.Red;
+            this.colorRed.Location = new System.Drawing.Point(122, 54);
+            this.colorRed.Name = "colorRed";
+            this.colorRed.Size = new System.Drawing.Size(20, 18);
+            this.colorRed.TabIndex = 9;
+            this.colorRed.UseVisualStyleBackColor = false;
+            // 
             // colorBlue
             // 
             this.colorBlue.BackColor = System.Drawing.Color.Blue;
-            this.colorBlue.Location = new System.Drawing.Point(25, 88);
+            this.colorBlue.Location = new System.Drawing.Point(70, 30);
             this.colorBlue.Name = "colorBlue";
             this.colorBlue.Size = new System.Drawing.Size(20, 18);
             this.colorBlue.TabIndex = 5;
             this.colorBlue.UseVisualStyleBackColor = false;
             // 
+            // colorGreen
+            // 
+            this.colorGreen.BackColor = System.Drawing.Color.Lime;
+            this.colorGreen.Location = new System.Drawing.Point(44, 54);
+            this.colorGreen.Name = "colorGreen";
+            this.colorGreen.Size = new System.Drawing.Size(20, 18);
+            this.colorGreen.TabIndex = 8;
+            this.colorGreen.UseVisualStyleBackColor = false;
+            // 
             // colorPink
             // 
             this.colorPink.BackColor = System.Drawing.Color.Fuchsia;
-            this.colorPink.Location = new System.Drawing.Point(25, 136);
+            this.colorPink.Location = new System.Drawing.Point(122, 30);
             this.colorPink.Name = "colorPink";
             this.colorPink.Size = new System.Drawing.Size(20, 18);
             this.colorPink.TabIndex = 6;
@@ -101,112 +259,44 @@
             // colorWhite
             // 
             this.colorWhite.BackColor = System.Drawing.Color.White;
-            this.colorWhite.Location = new System.Drawing.Point(51, 40);
+            this.colorWhite.Location = new System.Drawing.Point(18, 54);
             this.colorWhite.Name = "colorWhite";
             this.colorWhite.Size = new System.Drawing.Size(20, 18);
             this.colorWhite.TabIndex = 7;
             this.colorWhite.UseVisualStyleBackColor = false;
             // 
-            // colorGreen
+            // pictureBox1
             // 
-            this.colorGreen.BackColor = System.Drawing.Color.Lime;
-            this.colorGreen.Location = new System.Drawing.Point(51, 64);
-            this.colorGreen.Name = "colorGreen";
-            this.colorGreen.Size = new System.Drawing.Size(20, 18);
-            this.colorGreen.TabIndex = 8;
-            this.colorGreen.UseVisualStyleBackColor = false;
-            // 
-            // colorRed
-            // 
-            this.colorRed.BackColor = System.Drawing.Color.Red;
-            this.colorRed.Location = new System.Drawing.Point(50, 136);
-            this.colorRed.Name = "colorRed";
-            this.colorRed.Size = new System.Drawing.Size(20, 18);
-            this.colorRed.TabIndex = 9;
-            this.colorRed.UseVisualStyleBackColor = false;
-            // 
-            // colorOrange
-            // 
-            this.colorOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.colorOrange.Location = new System.Drawing.Point(50, 112);
-            this.colorOrange.Name = "colorOrange";
-            this.colorOrange.Size = new System.Drawing.Size(20, 18);
-            this.colorOrange.TabIndex = 10;
-            this.colorOrange.UseVisualStyleBackColor = false;
-            // 
-            // colorYellow
-            // 
-            this.colorYellow.BackColor = System.Drawing.Color.Yellow;
-            this.colorYellow.Location = new System.Drawing.Point(50, 88);
-            this.colorYellow.Name = "colorYellow";
-            this.colorYellow.Size = new System.Drawing.Size(20, 18);
-            this.colorYellow.TabIndex = 11;
-            this.colorYellow.UseVisualStyleBackColor = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 13);
-            this.label1.TabIndex = 12;
-            this.label1.Text = "vyber si barvu";
-            // 
-            // hScrollBar1
-            // 
-            this.hScrollBar1.Location = new System.Drawing.Point(15, 202);
-            this.hScrollBar1.Name = "hScrollBar1";
-            this.hScrollBar1.Size = new System.Drawing.Size(143, 25);
-            this.hScrollBar1.TabIndex = 13;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 179);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 13);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "tloušťka čáry";
-            // 
-            // newPage
-            // 
-            this.newPage.Location = new System.Drawing.Point(104, 88);
-            this.newPage.Name = "newPage";
-            this.newPage.Size = new System.Drawing.Size(53, 41);
-            this.newPage.TabIndex = 15;
-            this.newPage.Text = "nový papír";
-            this.newPage.UseVisualStyleBackColor = true;
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 102);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(956, 505);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(980, 659);
-            this.Controls.Add(this.newPage);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.hScrollBar1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.colorYellow);
-            this.Controls.Add(this.colorOrange);
-            this.Controls.Add(this.colorRed);
-            this.Controls.Add(this.colorGreen);
-            this.Controls.Add(this.colorWhite);
-            this.Controls.Add(this.colorPink);
-            this.Controls.Add(this.colorBlue);
-            this.Controls.Add(this.colorPurple);
-            this.Controls.Add(this.colorCyan);
-            this.Controls.Add(this.colorBlack);
-            this.Controls.Add(this.drawingpanel);
+            this.ClientSize = new System.Drawing.Size(980, 613);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.optionsPanel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.optionsPanel.ResumeLayout(false);
+            this.optionsPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel drawingpanel;
+        private System.Windows.Forms.Panel optionsPanel;
         private System.Windows.Forms.Button colorBlack;
         private System.Windows.Forms.Button colorCyan;
         private System.Windows.Forms.Button colorPurple;
@@ -220,7 +310,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.HScrollBar hScrollBar1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button newPage;
+        private System.Windows.Forms.Button newPageButton;
+        private System.Windows.Forms.Button buttonCircle;
+        private System.Windows.Forms.Button buttonEraser;
+        private System.Windows.Forms.Button buttonPen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonRectangle;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- MalovaniUkol/MalovaniUkol/Form1.cs
+++ MalovaniUkol/MalovaniUkol/Form1.cs
@@ -15,8 +15,121 @@ namespace MalovaniUkol
         public Form1()
         {
             InitializeComponent();
+
+            this.Width = 900;
+            this.Height = 700;
+            bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            pictureBox1.Image = bm;
+        }
+
+        Bitmap bm;
+        Graphics g;
+        bool paint = false;
+        Point px, py;
+        Pen myPen = new Pen(Color.Black, 1);
+        Pen Eraser = new Pen(Color.White, 25); //guma má bílou barvu a danou velikost 25px
+        int index; //index na volbu kreslení - pero, guma, kruh, čtverec
+        int x, y, fX, fY, sX, sY; //pro kreslení objektů
+        //x - aktuální poloha myši na souřadnici x
+        //y - aktuální poloha myši na souřadnici y
+        //sX, sY - počáteční body kreslení, odkud zmáčknu myš - startX, startY
+        //fX - finální šírka objektu - final souřadnice x
+        //fY - finální výška objektu - final souřadnice y
+
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true; //pokud držím stisknoutou myš tak true
+            py = e.Location;
+
+            sX = e.Location.X;  //na kreslení objektů
+            sY = e.Location.Y;  //když zmáčknu myš, tak ty body uloží jako počáteční body kreslení
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) //sleduje pohyb myši
+        {
+            if (paint == true) //pokud je paint true - (mám zmáčklou myš), tak povolí akci -> kreslení, gumování...
+            {
+                if (index == 1)
+                {
+                    px = e.Location; //aktuální pozice myši uložená do px
+                    g.DrawLine(myPen, px, py); //nakreslí čáru mezi px a py
+                    py = px; //nastaví novou hodnotu py na aktální polohu myši aby se dalo kreslit dál
+                }
+                else if (index == 2)
+                {
+                    px = e.Location; //to samé jako u pera, akorát použiju pero, -->
+                    g.DrawLine(Eraser, px, py); // --> co má barvu nastavenou na bílou a tím to přemaluju
+                    py = px;
+                }
+            }
+            pictureBox1.Refresh(); //znovu vykreslí paintbox a rím je vidět co malujeme
+
+            x = e.Location.X; //aktuální poloha myši na souřadnici x
+            y = e.Location.Y; //aktuální poloha myši na souřadnici y
+            fX = e.Location.X - sX; //finální šírka objektu - final souřadnice x
+            fY = e.Location.Y - sY; //finální výška objektu - final souřadnice y
         }
 
         
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            paint = false; //jakmile pustim myš a už jí nedržim stisklou tak false
+
+
+            fX = x - sX; //vypočítání finálních souřadnic -> aktuální souřadnice mínus původní souřadnice
+            fY = y - sY;
+
+            if (index == 3)
+            {
+                g.DrawEllipse(myPen, sX, sY, fX, fY); //kruh
+            }               
+            else if (index == 4)                            //čtverec a kruh stejným způsobem
+            {
+                g.DrawRectangle(myPen, sX, sY, fX, fY); //čtverec
+            }
+        }
+        private void newPageButton_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Image = bm;
+            index = 0;
+        }
+        private void buttonPen_Click(object sender, EventArgs e)
+        {
+            index = 1;
+        }
+        private void buttonEraser_Click(object sender, EventArgs e)
+        {
+            index = 2;
+        }
+        private void buttonCircle_Click(object sender, EventArgs e)
+        {
+            index = 3;
+        }
+        private void buttonRectangle_Click(object sender, EventArgs e)
+        {
+            index = 4;
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e) //dělá mi nákres pri kerslení objektů
+        {
+            Graphics g = e.Graphics;
+            if (paint == true)
+            {
+                if (index == 3)
+                {
+                    g.DrawEllipse(myPen, sX, sY, fX, fY); //kruh
+                }
+                else if (index == 4)                            //čtverec a kruh stejným způsobem
+                {
+                    g.DrawRectangle(myPen, sX, sY, fX, fY); //čtverec
+                }
+            }
+        }
+
+
     }
 }
